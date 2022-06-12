bd873b86714655e06fe9953307d41b2d2347ea3c
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-12 00:28:15 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Miguel Quezada on 12-06-22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
